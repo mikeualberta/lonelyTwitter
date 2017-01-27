6963a0b7789a41d6a933bb2d203ee469d193396a
mikeualberta <simion@ualberta.ca>
2017-01-26 18:55:19 -0700

    Attempt to upload lab 3

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,16 +1,19 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,13 +21,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
-	/** Called when the activity is first created. */
+
+	private ArrayList<Tweet> tweetList;
+	private ArrayAdapter<Tweet> adapter;
+
+
+
+	/**
+	 * Called when the activity is first created.
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,83 +57,101 @@ public class LonelyTwitterActivity extends Activity {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
 
-				/*Mood mood = new HappyMood("Sad");
-				Mood happyMood = new SadMood("Happy");*/
 
-				Tweet tweet = new ImportantTweet("test string"); /* Creating new instance of tweet */
-				Tweet normalTweet = new NormalTweet("test string");
+				Tweet tweet = new NormalTweet(text);
 
-				/* Need try and catch block to account for the exception */
-				try{
-					if (tweet.isImportant())
-						tweet.setMessage("better string");
-					/* Private attribute cannot be accessed directly so you have to use a setter */
-				}catch(Exception e){
-					throw new RuntimeException(); /* Used when you don't know exactly the best solution */
-
-				}
-				String string = tweet.getMessage();
-
-
-				ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+//				tweetList = new ArrayList<Tweet>();
 				tweetList.add(tweet);
-				tweetList.add(normalTweet);
 
-				/*ArrayList<Mood> moodList = new ArrayList<Mood>();
-				moodList.add(mood);
-				moodList.add(happyMood);*/
+				adapter.notifyDataSetChanged();
 
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				saveInFile();
 
 			}
 		});
+
+
+		/* NEW STUFF*/
+		Button clearButton = (Button) findViewById(R.id.clear);
+
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+//				String text = bodyText.getText().toString();
+				tweetList.clear();
+
+//				Tweet tweet = new NormalTweet(text);
+//
+//				tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+
+			}
+		});
+
+		/* End of NEW STUFF */
+
 	}
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
-		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+		super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+//		String[] tweets = loadFromFile();
+		loadFromFile();
+//		tweetList = new ArrayList<Tweet>();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+
+			Gson gson = new Gson();
+
+			tweetList = gson.fromJson(in, new TypeToken<ArrayList<NormalTweet>>() {
+			}.getType());
+
+//			logs = gson.fromJson(br, new TypeToken<List<JsonLog>>(){}.getType());
+			fis.close();
 
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			tweetList = new ArrayList<Tweet>();
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+//		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date) {
+
+	private void saveInFile() {
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+					Context.MODE_PRIVATE);
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+			Gson gson = new Gson();
+
+			gson.toJson(tweetList, out);
+
+			out.flush();
+
 			fos.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		}
 	}
+
 }
