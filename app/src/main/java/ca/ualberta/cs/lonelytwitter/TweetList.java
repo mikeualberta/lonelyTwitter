933f580f0aa238a816ac92938d1361d289c01684
mikeualberta <simion@ualberta.ca>
2017-02-16 18:16:00 -0700

    completed lab 6

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,42 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by simion on 2/16/17.
+ */
+
+public class TweetList {
+    private ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+
+    public void add(NormalTweet tweet){
+        if(this.hasTweet(tweet)){
+            throw new IllegalArgumentException();
+        }
+        else{
+            tweets.add(tweet);
+        }
+
+    }
+
+    public boolean hasTweet(NormalTweet tweet){
+        return tweets.contains(tweet);
+    }
+
+    public NormalTweet getTweet(int index){
+        return tweets.get(0);
+    }
+
+    public void delete(NormalTweet tweet){
+        tweets.remove(tweet);
+    }
+
+    public int getCount(){
+        return tweets.size();
+    }
+
+    public ArrayList<NormalTweet> getTweets(){
+        return tweets;
+    }
+}
